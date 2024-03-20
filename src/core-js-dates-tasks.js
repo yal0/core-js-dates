6fcefc74d91390fcd9bd0e6a3431b6e32d6315a9
Olga Zhuravleva <onpol@mail.ru>
2024-03-20 13:31:06 +0300

    feat: 14/14 complete

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -234,8 +234,21 @@ function getQuarter(date) {
  * { start: '01-01-2024', end: '15-01-2024' }, 1, 3 => ['01-01-2024', '05-01-2024', '09-01-2024', '13-01-2024']
  * { start: '01-01-2024', end: '10-01-2024' }, 1, 1 => ['01-01-2024', '03-01-2024', '05-01-2024', '07-01-2024', '09-01-2024']
  */
-function getWorkSchedule(/* period, countWorkDays, countOffDays */) {
-  throw new Error('Not implemented');
+function getWorkSchedule(period, countWorkDays, countOffDays) {
+  const startDate = new Date(period.start.split('-').reverse().join('-'));
+  const endDate = new Date(period.end.split('-').reverse().join('-'));
+  const periodDays = (endDate - startDate) / (24 * 60 * 60 * 1000);
+  const result = [];
+  for (let i = 0; i <= periodDays; i += countWorkDays + countOffDays) {
+    for (let j = 0; j < countWorkDays && i + j <= periodDays; j += 1) {
+      const date = new Date(startDate);
+      date.setDate(date.getDate() + i + j);
+      result.push(
+        `${String(date.getDate()).padStart(2, '0')}-${String(date.getMonth() + 1).padStart(2, '0')}-${date.getFullYear()}`
+      );
+    }
+  }
+  return result;
 }
 
 /**
